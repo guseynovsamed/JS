20a2994fb214384629082acd6b226fb9f899ac08
guseynovsamed <samadaghash@code.edu.az>
2024-01-28 00:27:01 +0400

    JS

---
+++ assets/js/script.js
@@ -0,0 +1,138 @@
+"use strict"
+
+
+
+// var surname = "Huseynov";
+// console.log(surname);
+// let address = "Ehmedli";
+// console.log(address);
+// var name = "Semed";
+// const age = 27;
+// console.log(age);
+// let isMarried=true;
+// console.log(isMarried);
+// name="salam";
+// console.log(name); 
+
+// let surname = 55;
+// console.log(null);
+
+
+// let isMarried = true;
+
+// isMarried=100;
+// console.log(isMarried);
+
+
+// const price = 100;
+
+// price +=50
+
+// console.log(price);
+
+// let stu ={
+//     name:"Semed",
+//     surname:"Huseynov",
+//     age:27
+// };
+
+// console.log(stu.name+"-"+stu.surname)
+
+//  let name = "XXX";
+
+//  let surname = "YYY";
+
+//  let fullname=`${name}${surname}`;
+
+//  console.log(fullname);
+
+
+// let ages = [22,33,44,8,"salam"];
+
+// console.log(ages[ages.length-1]);
+
+//  let num1 = 100;
+//  let num2 = 30;
+
+//  let result=num1+num2;
+
+//  console.log(result);
+
+// let num1="salam";
+
+// let num2 =55;
+
+// let result=num1+num2;
+
+// console.log(result);
+
+// let num =100;
+
+// num*=2;
+
+// console.log(num);
+
+
+// let num1 = "55";
+
+// let num2 = 55;
+
+// console.log(num1===num2)
+
+
+// if(number > 90 && number<120){
+//     console.log("yes")
+// }else{
+//     console.log("no");
+// }
+
+// let number = 100;
+
+
+// let result = number == 100 ? "yes":"no";
+
+// console.log(result);
+
+// let weekDay = 3;
+
+// switch (weekDay) {
+//     case 3:
+//             console.log("Wednesday");
+//         break;
+//         case 1:
+//             console.log("Monday");
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let isMarried = false;
+
+// if(isMarried)
+// {
+//     console.log("Yes")
+// }else{
+//     console.log("No")
+// };
+
+// let nums = [4,5,6,7,30]
+
+// if(nums[nums.length-1]===30){
+//     console.log("Beraderdir");
+// }else{
+//     console.log("Beraber deil");
+// }
+
+
+let nums = [1,2,35,6,7]
+let sum = 0;
+
+
+for(let i=0 ; i<nums.length;i++){
+    
+    sum+=i;
+}
+console.log(i)
+
+
